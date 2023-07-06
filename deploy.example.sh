8e3143f984b7086a70a092999773488524b31710
pinkiepie <pinkipies0912@gmail.com>
2023-07-07 02:44:54 +0900

    feature: Add serverless yml

---
+++ deploy.example.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# For local
+
+export ROLE=
+export ECR_URL=
+export REPO=
+export TAG=
+export ECR_URI=${ECR_URL}/${REPO}:${TAG}
+export SUBNET_GROUP_ID=
+export SUBNET_ID=
+
+export AUTOSCALING_ENV_TAGS=
+export AUTOSCALING_NAME_TAGS=
+export EC2_NAME_TAGS=
+export EC2_ENV_TAGS=
+export RDS_IDENTIFIER=
+
+# Build image
+docker build -t ${REPO}:${TAG} .
+
+# Login to ECR
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${ECR_URL} 
+
+# Tag
+docker tag ${REPO}:${TAG} ${ECR_URI}
+
+# Push to ECR
+docker push ${ECR_URI}
+
+# Deploy lambda
+sls deploy
