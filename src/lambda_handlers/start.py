3a15a33e5452a0a6cc0c9dde55f3f67a8db05e91
pinkiepie <pinkipies0912@gmail.com>
2023-07-07 00:51:08 +0900

    feature: Add lambda_handlers

---
+++ src/lambda_handlers/start.py
@@ -0,0 +1,22 @@
+from config import load_from_env
+from infra_handlers.auto_scalining import AutoScalingGroupHandler
+from infra_handlers.ec2 import EC2Handler
+from infra_handlers.rds import RdsHandler
+
+
+def stop_handler(event, context):
+    conf = load_from_env()
+
+    auto_scalining_handler = AutoScalingGroupHandler(conf.region)
+    ec2_handler = EC2Handler(conf.region)
+    rds_handler = RdsHandler(conf.region)
+
+    auto_scalining_handler.change_scale(
+        name_tags=conf.autoscaling_name_tags,
+        env_tags=conf.autoscaling_env_tags,
+        target_cnt=1,
+    )
+    ec2_handler.start_instances(
+        name_tags=conf.ec2_name_tags, env_tags=conf.ec2_env_tags
+    )
+    rds_handler.start(rds_identifier=conf.rds_identifier)
---
+++ src/lambda_handlers/stop.py
@@ -0,0 +1,20 @@
+from config import load_from_env
+from infra_handlers.auto_scalining import AutoScalingGroupHandler
+from infra_handlers.ec2 import EC2Handler
+from infra_handlers.rds import RdsHandler
+
+
+def stop_handler(event, context):
+    conf = load_from_env()
+
+    auto_scalining_handler = AutoScalingGroupHandler(conf.region)
+    ec2_handler = EC2Handler(conf.region)
+    rds_handler = RdsHandler(conf.region)
+
+    auto_scalining_handler.change_scale(
+        name_tags=conf.autoscaling_name_tags,
+        env_tags=conf.autoscaling_env_tags,
+        target_cnt=0,
+    )
+    ec2_handler.stop_instances(name_tags=conf.ec2_name_tags, env_tags=conf.ec2_env_tags)
+    rds_handler.stop(rds_identifier=conf.rds_identifier)
